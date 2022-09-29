38c98d343e6ee95cff582b2794a0592f4b3fba2d
Armano Barros Alves Junior <armano.barros20@gmail.com>
2022-09-29 13:38:48 -0300

    Voltando para outra versão

--- app.js
+++ app.js
@@ -1,5 +1,2 @@
 console.log("Rodando o sistema de gerenciamente de jogos");
 console.log("Aplicação está se conectando ao banco de dados");
-// Mais um console log de teste:
-console.log("O Paulo fez esse commit sem você ver na aula!");
-console.log("Enviando para o GitHub");
