fb2f0c7772c93a76090fd3779c70288521dfab5b
Armano Barros Alves Junior <armano.barros20@gmail.com>
2022-10-03 10:54:33 -0300

    corrigindo o app.js
    !

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodando o sistema de gerenciamente de jogos");
 console.log("Aplicação está se conectando ao banco de dados");
 //Alterações
 console.log("Demostrando alteração");
+console.log("Teste")
